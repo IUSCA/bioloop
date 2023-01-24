08ae2b6eee3847d31b1881e9aa841352f0a45ff0
dgluser <dgluser@dglservice1.sca.iu.edu>
2023-01-24 22:52:27 +0000

    update env var and docker-compose.yml

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/health', (req, res) => {
+  res.send('OK')
+})
+
 router.use('/batch', require('./batch'));
 
 module.exports = router;
