64ea76ae8e9e0e249f53f77474bce484c5189837
dgluser <dgluser@dglservice1.sca.iu.edu>
2023-01-25 17:38:25 +0000

    api docker deployment changes

--- api/index.js
+++ api/index.js
@@ -5,8 +5,9 @@ const app = require('./app');
 const logger = require('./logger');
 
 const port = config.get('express.port');
+const host = config.get('express.host')
 app.listen(port, () => {
   /* eslint-disable no-console */
-  logger.info(`Listening: http://localhost:${port}`);
+  logger.info(`Listening: http://${host}:${port}`);
   /* eslint-enable no-console */
 });
