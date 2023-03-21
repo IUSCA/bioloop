1aa0afd633e0113f528bd9859ba0a7787005b4f0
Deepak Duggirala <ddeepak6992@gmail.com>
2023-03-21 09:43:03 -0400

    add toast service for notifications

---
+++ ui/src/services/toast.js
@@ -0,0 +1,34 @@
+import { useToast } from "vuestic-ui";
+const { init, close, closeAll } = useToast();
+
+class Toast {
+  success(message) {
+    init({
+      message,
+      position: "bottom-right",
+      color: "success",
+    });
+  }
+  error(message) {
+    init({
+      message,
+      position: "bottom-right",
+      color: "danger",
+    });
+  }
+  info(message) {
+    init({
+      message,
+      position: "bottom-right",
+      color: "primary",
+    });
+  }
+  close() {
+    close;
+  }
+  closeAll() {
+    closeAll;
+  }
+}
+
+export default new Toast();
