78cfe777d1020987f265aba5858538dc99cd6259
Deepak Duggirala <ddeepak6992@gmail.com>
2023-05-11 19:09:43 -0400

    remove dotmap dependency in workers as it causing problems converting to dict

--- workers/workers/config/__init__.py
+++ workers/workers/config/__init__.py
@@ -2,7 +2,6 @@ import importlib
 import os
 
 from dotenv import load_dotenv
-from dotmap import DotMap
 
 from workers import utils
 from workers.config import common
@@ -12,6 +11,6 @@ load_dotenv()  # take environment variables from .env.
 env = os.environ.get('APP_ENV', None)
 if env:
     env_module = importlib.import_module(f'workers.config.{env}')
-    config = DotMap(utils.merge(common.config, env_module.config))
+    config = utils.merge(common.config, env_module.config)
 else:
-    config = DotMap(common.config)
+    config = common.config
