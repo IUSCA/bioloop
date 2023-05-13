40a86453bf5abc7104d112bc7a0a83e46df21e30
Deepak Duggirala <ddeepak6992@gmail.com>
2023-05-13 11:59:54 -0400

    update celery to 5.3.0rc1 and update its config to reliably run long running tasks

--- workers/ecosystem.config.js
+++ workers/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: "celery_worker",
       script: "python",
-      args: "-m celery -A workers.celery_app worker --concurrency 8 --loglevel INFO",
+      args: "-m celery -A workers.celery_app worker --concurrency 8 --loglevel INFO -O fair",
       watch: false,
       interpreter: "",
       log_date_format: "YYYY-MM-DD HH:mm Z",
--- workers/workers/config/celeryconfig.py
+++ workers/workers/config/celeryconfig.py
@@ -13,17 +13,7 @@ mongo_username = os.environ['MONGO_USER']
 mongo_password = os.environ['MONGO_PASS']
 
 # https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/rabbitmq.html
-# broker_url = f'amqp://dgl:{urllib.parse.quote(queue_password)}@commons3.sca.iu.edu:5672/dgl-tests'
 broker_url = f'amqp://{queue_username}:{urllib.parse.quote(queue_password)}@{queue_url}'
-# task_routes = {
-#     'tasksB.task2': 'subtractqueue'
-# }
-
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
-result_extended = True
-
-task_serializer = 'json'
-result_serializer = 'json'
 
 # https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/redis.html#results
 # result_backend = 'redis://localhost:6379/0'
@@ -34,3 +24,57 @@ result_backend = f'mongodb://{mongo_username}:{urllib.parse.quote(mongo_password
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#database-backend-settings
 # https://stackoverflow.com/questions/69952488/celery-task-result-in-postgres-database-is-in-byte-format
 # result_backend = 'db+postgresql://username:password@localhost:5432/celery'
+
+# If enabled, backend will try to retry on the event of recoverable exceptions instead of propagating the exception.
+# It will use an exponential backoff sleep time between 2 retries.
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-backend-always-retry
+result_backend_always_retry = True
+result_backend_max_sleep_between_retries_ms = 10000
+result_backend_base_sleep_between_retries_ms = 1000
+result_backend_max_retries = 10
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
+result_extended = True
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-serializer
+result_serializer = 'json'
+
+# task entries in database are persisted forever
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-expires
+result_expires = 0
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-serializer
+task_serializer = 'json'
+
+# If True the task reports its status as ‘started’ when the task is executed by a worker.
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-track-started
+task_track_started = True
+
+# The worker processing the task will be killed and replaced with a new one when this is exceeded.
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-time-limit
+ONE_DAY = 24 * 60 * 60
+task_time_limit = ONE_DAY
+
+# The SoftTimeLimitExceeded exception will be raised when this is exceeded.
+# The task can catch this to clean up before the hard time limit.
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-soft-time-limit
+TEN_MINUTES = 10 * 60
+task_soft_time_limit = ONE_DAY - TEN_MINUTES
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-routes
+# task_routes = {
+#     'tasksB.task2': 'subtractqueue'
+# }
+
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-acks-late
+task_acks_late = True
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-reject-on-worker-lost
+task_reject_on_worker_lost = True
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-prefetch-multiplier
+worker_prefetch_multiplier = 1
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-state-db
+worker_state_db = "./celery-state.db"
