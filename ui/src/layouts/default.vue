0cba5b10c741fe9b57d063fa60124eafad92618c
Rishi Pandey <ripandey29@gmail.com>
2023-07-31 19:16:41 -0400

    collapse sidebar when screen width goes below md

--- ui/src/layouts/default.vue
+++ ui/src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <p>Breakpoint: {{ breakpoint.current }}</p>
-  <p>isSidebarCollapsed: {{ isSidebarCollapsed }}</p>
+  <!-- <p>Breakpoint: {{ breakpoint.current }}</p>
+  <p>isSidebarCollapsed: {{ isSidebarCollapsed }}</p> -->
   <Header 
     :isSidebarCollapsed="isSidebarCollapsed"
     @toggle-sidebar-visibility="toggleSidebarVisibility"
@@ -24,31 +24,16 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import { useBreakpoint } from "vuestic-ui"
 
 const breakpoint = useBreakpoint()
 
-// const isSidebarOpen = !(breakpoint.current !== 'xl' && breakpoint.current !== 'lg' && breakpoint.current !== 'md')
+let isSidebarCollapsed = ref(false)
 
-// const isSidebarCollapsed = ref(isSidebarOpen)
-
-// const isSidebarOpen = ref(false)
-
-onMounted(() => {
-  console.log(`breakpoint.current = ${breakpoint.current}`)
-  console.log(`breakpoint.current !== 'xl' && breakpoint.current !== 'lg' && breakpoint.current !== 'md' = ${breakpoint.current !== 'xl' && breakpoint.current !== 'lg' && breakpoint.current !== 'md'}`)
-  console.log(`isSidebarCollapsed = ${isSidebarCollapsed.value}`)
+watch(() => breakpoint.current, () => {
+  isSidebarCollapsed.value = breakpoint.current !== 'xl' && breakpoint.current !== 'lg' && breakpoint.current !== 'md'
 })
-
-const isSidebarCollapsed = computed({
-  get() {
-    return breakpoint.current !== 'lg' && breakpoint.current !== 'md' && breakpoint.current !== 'xl'
-  }
-  // , set(newValue) {
-    
-  // }
-  })
 
 const toggleSidebarVisibility = () => {
   isSidebarCollapsed.value = !isSidebarCollapsed.value
