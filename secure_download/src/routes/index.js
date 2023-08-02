a2e7afbe6ee100c5f6f56d0e23ebce0bd76cbd4b
Center for Proteome Analysis <cpauser@colo25.carbonate.uits.iu.edu>
2023-08-02 17:01:14 -0400

    turn off nginx response buffering to prevent large file download failures

--- secure_download/src/routes/index.js
+++ secure_download/src/routes/index.js
@@ -31,7 +31,14 @@ router.get('*', (req, res, next) => {
 
   if (req_path === token_file_path) {
     res.set('X-Accel-Redirect', `/data/${token_file_path}`);
+
+    // make browser download response instead of attempting to render it
     res.set('content-type', 'application/octet-stream; charset=utf-8');
+
+    // makes nginx not cache the response file
+    // otherwise the response cuts off at 1GB as the max buffer size is reached and the file download fails
+    // https://stackoverflow.com/a/64282626
+    res.set('X-Accel-Buffering', 'no');
     res.send('');
   } else {
     return next(createError.Forbidden('Invalid path'));
