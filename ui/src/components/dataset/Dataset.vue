bbdb713d8f0874057232f48639927fb43ad46e1a
Rishi Pandey <ripandey29@gmail.com>
2023-08-23 10:56:31 -0400

    removed extra vertical gap provided to dataset view

--- ui/src/components/dataset/Dataset.vue
+++ ui/src/components/dataset/Dataset.vue
@@ -1,7 +1,7 @@
 <template>
   <va-inner-loading :loading="loading">
     <!-- Content -->
-    <div class="flex flex-col gap-3">
+    <div class="flex flex-col">
       <!-- Associated datasets -->
 
       <assoc-datasets
