206a80c4afb63911f2117b235fd8c23fb2a5e14b
Rishi Pandey <ripandey29@gmail.com>
2023-09-18 09:15:42 -0400

    added basic page/sidebar item for Stats

---
+++ ui/src/pages/stats.vue
@@ -0,0 +1,13 @@
+<template>Hello</template>
+
+<script setup>
+import { useNavStore } from "@/stores/nav";
+
+const nav = useNavStore();
+
+nav.setNavItems([
+  {
+    label: "Stats/Tracking",
+  },
+]);
+</script>
