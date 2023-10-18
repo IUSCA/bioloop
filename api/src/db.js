c715542ef9cab98bdea049848aa20760841685eb
Deepak Duggirala <ddeepak6992@gmail.com>
2023-10-17 23:39:30 -0400

    remove db details from config as they are moved into .env

--- api/src/db.js
+++ api/src/db.js
@@ -1,22 +1,22 @@
 require('dotenv-safe').config();
-const process = require('process');
-const config = require('config');
+// const process = require('process');
+// const config = require('config');
 
-function makeDatabaseUrl() {
-  const user = config.get('postgres.user');
-  const password = encodeURIComponent(config.get('postgres.password'));
-  const hostname = config.get('postgres.hostname');
-  const dbPort = config.get('postgres.port');
-  const db = config.get('postgres.db');
-  const schema = config.get('postgres.schema');
+// function makeDatabaseUrl() {
+//   const user = config.get('postgres.user');
+//   const password = encodeURIComponent(config.get('postgres.password'));
+//   const hostname = config.get('postgres.hostname');
+//   const dbPort = config.get('postgres.port');
+//   const db = config.get('postgres.db');
+//   const schema = config.get('postgres.schema');
 
-  return `postgresql://${user}:${password}@${hostname}:${dbPort}/${db}?schema=${schema}`;
-}
+//   return `postgresql://${user}:${password}@${hostname}:${dbPort}/${db}?schema=${schema}`;
+// }
 
-// set DATABASE_URL environment variable for prisma
-const dbUrl = makeDatabaseUrl();
-// console.log(dbUrl);
-process.env.DATABASE_URL = dbUrl;
+// // set DATABASE_URL environment variable for prisma
+// const dbUrl = makeDatabaseUrl();
+// // console.log(dbUrl);
+// process.env.DATABASE_URL = dbUrl;
 
 // Postgres columns can be of BigInt type for which serialization is not defined
 // eslint-disable-next-line no-extend-native, func-names
