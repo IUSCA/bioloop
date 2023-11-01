e6cdac2562adfe5bab7b974c957f30138dc0a685
Deepak Duggirala <ddeepak6992@gmail.com>
2023-11-01 05:59:04 -0400

    remove auth_state table

--- api/prisma/migrations/20231026201536_google_auth/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "auth_state" (
-    "state" TEXT NOT NULL,
-    "provider" TEXT NOT NULL,
-    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "auth_state_pkey" PRIMARY KEY ("state")
-);
