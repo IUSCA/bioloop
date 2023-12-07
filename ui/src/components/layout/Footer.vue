d11bbf5f2cc1e319f986182fc08ed810f536422b
Rishi Pandey <ripandey29@gmail.com>
2023-12-07 18:34:07 -0500

    added divider

--- ui/src/components/layout/Footer.vue
+++ ui/src/components/layout/Footer.vue
@@ -1,3 +1,7 @@
+<script>
+import config from "@/config";
+</script>
+
 <template>
   <footer
     id="footer-v1"
@@ -6,54 +10,60 @@
     itemType="http://schema.org/CollegeOrUniversity"
     class="text-sm"
   >
-    <div class="flex flex-row">
-      <div class="flex align-center justify-end mr-2.5">
-        <a class="max-w-max" href="https://www.iu.edu">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <polygon
-              fill="currentColor"
-              points="15.3 3.19 15.3 5 16.55 5 16.55 15.07 13.9 15.07 13.9 1.81 15.31 1.81 15.31 0 8.72 0 8.72 1.81 10.12 1.81 10.12 15.07 7.45 15.07 7.45 5 8.7 5 8.7 3.19 2.5 3.19 2.5 5 3.9 5 3.9 16.66 6.18 18.98 10.12 18.98 10.12 21.67 8.72 21.67 8.72 24 15.3 24 15.3 21.67 13.9 21.67 13.9 18.98 17.82 18.98 20.09 16.66 20.09 5 21.5 5 21.5 3.19 15.3 3.19"
-            />
-          </svg>
-        </a>
+    <va-divider />
+
+    <div class="flex flex-col text-center text-base">
+      <div>
+        <p>
+          Bioloop is a service of the Indiana University
+          <a href="https://sca.iu.edu">Scalable Compute Archive (IU SCA)</a>
+          group.
+        </p>
       </div>
 
-      <div class="flex align-center max-w-max mx-2.5">
-        <a
-          id="accessibility-link"
-          href="https://accessibility.iu.edu/assistance"
-          title="Having trouble accessing this web page content? Please visit this page for assistance."
-        >
-          Accessibility
-        </a>
-      </div>
-
-      <div class="flex align-center max-w-max mx-2.5">
-        <a id="privacy-policy-link" href="https://www.iu.edu/privacy">
-          Privacy Notice
-        </a>
-      </div>
-
-      <div class="flex align-center max-w-max ml-2.5">
-        <span>
-          <a href="https://www.iu.edu/copyright/index.html">Copyright </a>
-          ©{{ new Date().getFullYear() }} The Trustees of
-          <a href="https://www.iu.edu/" itemprop="url">
-            <span itemprop="name">Indiana University</span>
+      <div class="flex flex-row" id="links-container">
+        <div class="flex align-center justify-end mr-2.5">
+          <a class="max-w-max" href="https://www.iu.edu">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <polygon
+                fill="currentColor"
+                points="15.3 3.19 15.3 5 16.55 5 16.55 15.07 13.9 15.07 13.9 1.81 15.31 1.81 15.31 0 8.72 0 8.72 1.81 10.12 1.81 10.12 15.07 7.45 15.07 7.45 5 8.7 5 8.7 3.19 2.5 3.19 2.5 5 3.9 5 3.9 16.66 6.18 18.98 10.12 18.98 10.12 21.67 8.72 21.67 8.72 24 15.3 24 15.3 21.67 13.9 21.67 13.9 18.98 17.82 18.98 20.09 16.66 20.09 5 21.5 5 21.5 3.19 15.3 3.19"
+              />
+            </svg>
           </a>
-        </span>
-      </div>
+        </div>
 
-      <!--        <p class="copyright">-->
-      <!--          {{ config.appTitle }} is a service of the Indiana University-->
-      <!--          <a href="https://sca.iu.edu">Scalable Compute Archive (IU SCA)</a>-->
-      <!--          group.-->
-      <!--        </p>-->
+        <div class="flex align-center max-w-max mx-2.5">
+          <a
+            id="accessibility-link"
+            href="https://accessibility.iu.edu/assistance"
+            title="Having trouble accessing this web page content? Please visit this page for assistance."
+          >
+            Accessibility
+          </a>
+        </div>
+
+        <div class="flex align-center max-w-max mx-2.5">
+          <a id="privacy-policy-link" href="https://www.iu.edu/privacy">
+            Privacy Notice
+          </a>
+        </div>
+
+        <div class="flex align-center max-w-max ml-2.5">
+          <span>
+            <a href="https://www.iu.edu/copyright/index.html">Copyright </a>
+            ©{{ new Date().getFullYear() }} The Trustees of
+            <a href="https://www.iu.edu/" itemprop="url">
+              <span itemprop="name">Indiana University</span>
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -72,12 +82,11 @@
   padding-right: 12.8px;
 }
 
-#footer-v1 div {
+#links-container {
   height: 57px;
 }
 
 #footer-v1 p {
-  font-size: 0.6875em;
   /* 11px (11/16) */
 }
 
@@ -89,11 +98,5 @@
 
 #footer-v1 a:hover {
   text-decoration: underline;
-}
-
-@media screen and (min-width: 40em) {
-  #footer-v1 {
-    border-top: 1px solid #7a1705;
-  }
 }
 </style>
