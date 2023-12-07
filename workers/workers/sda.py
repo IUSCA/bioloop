01ee3371c44c50a3f369d10df1c46e763841a4d3
Ryan Long <ryanlong@iu.edu>
2023-12-07 13:53:16 -0500

    Refactor list_directory_recursively function

--- workers/workers/sda.py
+++ workers/workers/sda.py
@@ -85,12 +85,13 @@ def ensure_directory(dir_path: str) -> None:
     cmd.execute(command)
 
 def list_directory_recursively(dir_path: str, creds: dict = None) -> list[str]:
-    command = ['hsi', '-P', f'ls -R {dir_path}']
+
+    print(dir_path)
 
     if creds == None:
       command = ['hsi', '-P', f'ls -R {dir_path}']  
     else:
-      command = ['hsi', '-A', 'keytab', '-k', f'{creds["keytab"]}', '-l', f'{creds["username"]}', '-P', f'ls -R \"{dir_path}\"']
+      command = ['hsi', '-A', 'keytab', '-k', f'{creds["keytab"]}', '-l', f'{creds["username"]}', '-P', 'ls',  '-R',  f'{dir_path}']
 
     stdout, stderr = cmd.execute(command)
 
