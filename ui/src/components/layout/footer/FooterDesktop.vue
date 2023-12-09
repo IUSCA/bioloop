850f9338565bd0c494ab511646bcb8fe0886f9f4
Rishi Pandey <ripandey29@gmail.com>
2023-12-09 17:31:51 -0500

    changed orientation

--- ui/src/components/layout/footer/FooterDesktop.vue
+++ ui/src/components/layout/footer/FooterDesktop.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="flex flex-row">
-    <div class="flex justify-end mr-2.5">
-      <FooterLogo />
-    </div>
+  <div class="flex justify-center">
+    <div class="flex max-w-max flex-row">
+      <div class="max-w-max mr-2.5">
+        <FooterLogo />
+      </div>
 
-    <div class="max-w-max mx-2.5">
-      <FooterAccessibility />
-    </div>
+      <div class="max-w-max mx-2.5">
+        <FooterAccessibility />
+      </div>
 
-    <div class="max-w-max mx-2.5">
-      <FooterPrivacy />
-    </div>
+      <div class="max-w-max mx-2.5">
+        <FooterPrivacy />
+      </div>
 
-    <div class="max-w-max ml-2.5">
-      <FooterCopyright />
+      <div class="max-w-max ml-2.5">
+        <FooterCopyright />
+      </div>
     </div>
   </div>
 </template>
