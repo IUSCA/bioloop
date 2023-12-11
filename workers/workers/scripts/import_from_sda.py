c51a2c6fccc72068ddaa082b2fcb5cfdc4330431
Ryan Long <ryanlong@iu.edu>
2023-12-11 11:05:44 -0500

    Fix script path in import_from_sda.py

--- workers/workers/scripts/import_from_sda.py
+++ workers/workers/scripts/import_from_sda.py
@@ -123,7 +123,7 @@ def parse_output(input_string):
 
 
 def unzip_file(file_path):
-    command = [f'{script_dir}/extract.sh', file_path]
+    command = [f'{script_dir}extract.sh', file_path]
 
     stdout, stderr = cmd.execute(command)
 
