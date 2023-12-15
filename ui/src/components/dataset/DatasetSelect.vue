93e3ba289f10d44aa8ae676b8e15de9afb13b089
Rishi Pandey <ripandey29@gmail.com>
2023-12-14 19:44:45 -0500

    added bulk of functionality

--- ui/src/components/dataset/DatasetSelect.vue
+++ ui/src/components/dataset/DatasetSelect.vue
@@ -1,11 +1,28 @@
 <template>
   <AutoCompleteDynamic
+    class="dataset-select"
     :data="datasets"
     filter-by="name"
     placeholder="Search datasets by name"
     text-by="name"
     track-by="id"
-  />
+    @update-search="updateRetrievalQuery"
+  >
+    <template #filtered="{ item }">
+      <div class="flex">
+        <div>
+          <span> {{ item.name }} </span>
+          <span class="va-text-secondary p-1"> &VerticalLine; </span>
+          <span class="va-text-secondary text-sm"> {{ item.type }} </span>
+        </div>
+        <div class="text-right">
+          <span class="text-sm">
+            {{ formatBytes(item.du_size) }}
+          </span>
+        </div>
+      </div>
+    </template>
+  </AutoCompleteDynamic>
 
   <AutoComplete
     :data="datasets"
@@ -32,11 +49,30 @@
 <script setup>
 import datasetService from "@/services/dataset";
 import { formatBytes } from "@/services/utils";
-import AutoCompleteDynamic from "@/components/utils/AutoCompleteDynamic.vue";
+
+const DEFAULT_RETRIEVAL_QUERY = { sortBy: { name: "asc" }, limit: 5 };
 
 const datasets = ref([]);
+const retrievalQuery = ref(DEFAULT_RETRIEVAL_QUERY);
 
-datasetService.getAll().then((res) => {
-  datasets.value = res.data.datasets;
+const updateRetrievalQuery = (searchText) => {
+  retrievalQuery.value = {
+    ...retrievalQuery.value,
+    name: searchText || undefined,
+  };
+};
+
+const retrieveDatasets = () => {
+  datasetService.getAll(retrievalQuery.value).then((res) => {
+    datasets.value = res.data.datasets;
+  });
+};
+
+onMounted(() => {
+  retrieveDatasets();
+});
+
+watch(retrievalQuery, () => {
+  retrieveDatasets();
 });
 </script>
--- ui/src/components/utils/AutoCompleteDynamic.vue
+++ ui/src/components/utils/AutoCompleteDynamic.vue
@@ -2,30 +2,28 @@
   <va-form>
     <va-select
       v-model="selectedOption"
-      class="w-full"
+      class="w-full autocomplete"
       clearable
       :placeholder="props.placeholder"
       autocomplete
       :options="props.data"
       :text-by="props.textBy"
       :track-by="props.trackBy"
+      @updateSearch="updateSearch"
     >
       <template #option="{ option, selectOption }">
-        <va-button @click="selectOption(option)">
-          <div class="flex flex-row">
-            <div>{{ option.name }}</div>
-            <div>{{ option.type }}</div>
-            <div>{{ formatBytes(option.size) }}</div>
-          </div>
-        </va-button>
+        <div
+          class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 p-2 rounded"
+          @click="selectOption(option)"
+        >
+          <slot name="filtered" :item="option"> </slot>
+        </div>
       </template>
-    </va-select></va-form
-  >
+    </va-select>
+  </va-form>
 </template>
 
 <script setup>
-import { formatBytes } from "../../services/utils";
-
 const props = defineProps({
   placeholder: {
     type: String,
@@ -41,22 +39,35 @@ const props = defineProps({
   trackBy: {
     type: [String, Function],
   },
-  filterBy: {
-    type: String,
-    default: "value",
-  },
-  filterFn: {
-    type: Function,
-    default: null,
-  },
-  displayBy: {
-    type: String,
-    default: "name",
-  },
+  // filterBy: {
+  //   type: String,
+  //   default: "value",
+  // },
+  // filterFn: {
+  //   type: Function,
+  //   default: null,
+  // },
+  // displayBy: {
+  //   type: String,
+  //   default: "name",
+  // },
 });
 
-// const search = ref("");
+const emit = defineEmits(["select", "update-search"]);
+
+const updateSearch = (updatedSearchText) => {
+  debugger;
+  console.log(`updatedSearchText`);
+  console.log(`${updatedSearchText}`);
+  emit("update-search", updatedSearchText);
+};
+
 const selectedOption = ref({});
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.autocomplete {
+  // todo - does not work
+  //--va-select-dropdown-background: #ff13;
+}
+</style>
