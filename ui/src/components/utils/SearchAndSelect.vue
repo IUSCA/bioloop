6a95e457632fd87943e659a1a890e4365e957553
Rishi Pandey <ripandey29@gmail.com>
2024-01-31 16:19:59 -0500

    replaced icon slot with <va-input>'s clearable

--- ui/src/components/utils/SearchAndSelect.vue
+++ ui/src/components/utils/SearchAndSelect.vue
@@ -18,26 +18,12 @@
               :placeholder="props.placeholder || 'Type to search'"
               class="flex-auto"
               clearable
+              @clear="$emit('reset')"
             >
               <!-- Search icon -->
               <template #prependInner>
                 <va-icon class="text-xl" name="search" />
               </template>
-              <!--              &lt;!&ndash; Clear button &ndash;&gt;-->
-              <!--              <template #appendInner>-->
-              <!--                <va-popover message="Reset Filters">-->
-              <!--                  <va-button-->
-              <!--                    preset="plain"-->
-              <!--                    color="secondary"-->
-              <!--                    icon="highlight_off"-->
-              <!--                    @click="-->
-              <!--                      () => {-->
-              <!--                        $emit('reset');-->
-              <!--                      }-->
-              <!--                    "-->
-              <!--                  />-->
-              <!--                </va-popover>-->
-              <!--              </template>-->
             </va-input>
 
             <!-- Search filters -->
