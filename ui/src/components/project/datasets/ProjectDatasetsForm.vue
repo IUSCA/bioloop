3e48c1b9f7db8903968249cf3bfad970516b0e40
Rishi Pandey <ripandey29@gmail.com>
2024-02-06 12:11:18 -0500

    Revert "Revert "114 dynamic search (#122)" (#144)"
    This reverts commit 7af8f0b16f7efd85c9fffd5ac3b061a4fc09f5f1.

--- ui/src/components/project/datasets/ProjectDatasetsForm.vue
+++ ui/src/components/project/datasets/ProjectDatasetsForm.vue
@@ -1,33 +1,25 @@
 <template>
-  <div class="space-y-4">
-    <DatasetSelect @select="handleSelect" />
-
-    <div class="flex flex-row justify-between px-1">
-      <span class="text-lg font-bold tracking-wide">Datasets to assign</span>
-      <span class="text-right"
-        >{{ maybePluralize(projectFormStore.datasets.length, "dataset") }}
-      </span>
-    </div>
-
-    <ProjectDatasetsList
-      :datasets="projectFormStore.datasets"
-      show-remove
-      @remove="handleRemove"
-    />
-  </div>
+  <DatasetSelect
+    @select="handleSelect"
+    @remove="handleRemove"
+    :selected-results="projectFormStore.datasets"
+  />
 </template>
 
 <script setup>
-import { maybePluralize } from "@/services/utils";
 import { useProjectFormStore } from "@/stores/projects/projectForm";
 
 const projectFormStore = useProjectFormStore();
 
-function handleSelect(ds) {
-  projectFormStore.addDataset(ds);
+function handleSelect(datasets) {
+  for (const ds of datasets) {
+    projectFormStore.addDataset(ds);
+  }
 }
 
-function handleRemove(ds) {
-  projectFormStore.removeDataset(ds);
+function handleRemove(datasets) {
+  for (const ds of datasets) {
+    projectFormStore.removeDataset(ds);
+  }
 }
 </script>
