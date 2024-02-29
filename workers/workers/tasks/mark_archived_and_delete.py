38e352acea1fd3c045b1bc45c69635566f2d2d74
Rishi Pandey <96927993+ri-pandey@users.noreply.github.com>
2024-02-29 14:31:43 -0500

    102 bundle size usages (#183)
    * 102 - remove outdated usage
    
    * use arg
    
    ---------
    
    Co-authored-by: scadev <scadev@bioloop-dev1.sca.iu.edu>
    Co-authored-by: scadev@colo25 <scadev@colo25.carbonate.uits.iu.edu>

--- workers/workers/tasks/mark_archived_and_delete.py
+++ workers/workers/tasks/mark_archived_and_delete.py
@@ -10,10 +10,8 @@ def mark_archived_and_delete(celery_task, dataset_id, **kwargs):
     bundle = Path(dataset['bundle']['path'])
 
     sda_tar_path = f'archive/2023/raw_data/{dataset["name"]}.tar'
-    bundle_size = bundle.stat().st_size
     update_data = {
-        'archive_path': sda_tar_path,
-        'bundle_size': bundle_size
+        'archive_path': sda_tar_path
     }
     api.update_dataset(dataset_id=dataset_id, update_data=update_data)
     api.add_state_to_dataset(dataset_id=dataset_id, state='ARCHIVED')
