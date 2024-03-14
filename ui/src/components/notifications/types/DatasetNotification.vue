f455c3cf05d918fdf3db93c3f855edcc11f489d5
Rishi Pandey <ripandey29@gmail.com>
2024-03-14 16:34:19 -0400

    use individual notification type components

---
+++ ui/src/components/notifications/types/DatasetNotification.vue
@@ -0,0 +1,39 @@
+<template>
+  <a
+    class="notification-anchor"
+    :href="_notification.to || '#'"
+    @click="_notification.onClick"
+  >
+    <h6 class="va-h6">{{ _notification.label }}</h6>
+    <p>{{ _notification.text }}</p>
+  </a>
+</template>
+
+<script setup>
+import notificationService from "@/services/notification";
+
+const props = defineProps({
+  notification: {
+    type: Object,
+    required: true,
+  },
+});
+
+const _notification = computed(() => ({
+  ...props.notification,
+  to: `/duplicateDatasets/resolveDuplicates/${props.notification.dataset_action_items[0].id}`,
+  onClick: () => {
+    // change status of notification to ACK'd
+    return notificationService.updateNotificationStatus({
+      notification_id: props.notification.id,
+      status: "ACKNOWLEDGED",
+    });
+  },
+}));
+</script>
+
+<style lang="scss" scoped>
+.notification-anchor {
+  color: var(--va-text-primary);
+}
+</style>
