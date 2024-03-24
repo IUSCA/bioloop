4a3f2719885eabd3a6576d19512236d5602c64b5
Rishi Pandey <ripandey29@gmail.com>
2024-03-24 18:44:38 -0400

    logging

--- api/src/services/dataset.js
+++ api/src/services/dataset.js
@@ -560,6 +560,9 @@ async function validate_duplication_state(duplicate_dataset_id) {
     },
   });
 
+  console.log('duplicate_dataset:');
+  console.dir(duplicate_dataset, { depth: null });
+
   if (!duplicate_dataset.is_duplicate) {
     throw new Error(`Expected dataset ${duplicate_dataset.id} to be a duplicate, but it is not.`);
   }
@@ -615,6 +618,9 @@ async function validate_duplication_state(duplicate_dataset_id) {
  */
 async function validate_state_before_original_dataset_resource_purge(duplicate_dataset_id) {
   const { duplicate_dataset, original_dataset } = validate_duplication_state(duplicate_dataset_id);
+
+  console.log('duplicate_dataset:');
+  console.dir(duplicate_dataset, { depth: null });
 
   // throw error if this dataset is not ready for acceptance or rejection yet,
   // or if it is not already undergoing accetance.
