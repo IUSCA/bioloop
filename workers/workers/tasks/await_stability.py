a484a2cf270b44f964cc1b1a307b832ad266ce67
scadev@colo25 <scadev@colo25.carbonate.uits.iu.edu>
2024-04-03 21:33:31 -0400

    fixed utils

--- workers/workers/tasks/await_stability.py
+++ workers/workers/tasks/await_stability.py
@@ -9,7 +9,7 @@ from celery.utils.log import get_task_logger
 import workers.api as api
 import workers.config.celeryconfig as celeryconfig
 from workers.config import config
-import workers.utils as utils
+from workers.utils import is_dataset_locked_for_writes
 
 logger = get_task_logger(__name__)
 
@@ -45,7 +45,7 @@ def update_progress(celery_task, mod_time, delta):
 def await_stability(celery_task, dataset_id, wait_seconds: int = None, **kwargs):
     dataset = api.get_dataset(dataset_id=dataset_id)
 
-    locked, latest_state = utils.is_dataset_locked_for_writes(dataset)
+    locked, latest_state = is_dataset_locked_for_writes(dataset)
     if locked:
         raise Exception(f"Dataset {dataset['id']} is locked for writes. Dataset's current "
                         f"state is {latest_state}.")
