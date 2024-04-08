4983124d284762edf1fff3aa15c59090c765edfc
Rishi Pandey <ripandey29@gmail.com>
2024-04-08 13:48:21 -0400

    deleted migrations

--- api/prisma/migrations/20240318015145_173_added_cols_to_action_items/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "dataset_action_item" ADD COLUMN     "text" TEXT,
-ADD COLUMN     "title" TEXT,
-ADD COLUMN     "to" TEXT;
--- api/prisma/migrations/20240323000206_173_made_dataset_relation_mandatory/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `dataset_id` on table `dataset_action_item` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "dataset_action_item" ALTER COLUMN "dataset_id" SET NOT NULL;
