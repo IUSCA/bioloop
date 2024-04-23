6f53f0326e0d347323bba325e80517e6d4f3a4d3
Rishi Pandey <ripandey29@gmail.com>
2024-04-22 20:20:49 -0400

    188 - start disabling features

--- ui/src/components/dataset/DatasetInfo.vue
+++ ui/src/components/dataset/DatasetInfo.vue
@@ -43,7 +43,7 @@
           <td>Files</td>
           <td>{{ props.dataset.num_files }}</td>
         </tr>
-        <tr>
+        <tr v-if="featureFlags.genome_files.enabled">
           <td>Genome Files</td>
           <td>{{ props.dataset.metadata?.num_genome_files }}</td>
         </tr>
@@ -67,10 +67,11 @@
 <script setup>
 import { formatBytes } from "@/services/utils";
 import * as datetime from "@/services/datetime";
+import { useFeatureFlagStore } from "@/stores/featureFlag";
 
 const props = defineProps({ dataset: Object });
 
-console.log("in dataset info");
+const featureFlags = useFeatureFlagStore();
 </script>
 
 <style lang="scss" scoped>
