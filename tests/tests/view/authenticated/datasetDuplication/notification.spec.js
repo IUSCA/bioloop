80ded12c3ecb39c0af1308eb0c9f61ce1987b661
Rishi Pandey <ripandey29@gmail.com>
2024-05-01 15:47:55 -0400

    173 - use Playwright's APIRequest for API calls

--- tests/tests/view/authenticated/datasetDuplication/notification.spec.js
+++ tests/tests/view/authenticated/datasetDuplication/notification.spec.js
@@ -1,6 +1,7 @@
-import https from 'node:https';
-import adminApi from '../../../../services/adminApi';
+// import { request } from '@playwright/test';
+// import adminApi from '../../../../services/adminApi';
 // const axios = require('axios');
+
 const config = require('config');
 
 const { test, expect } = require('@playwright/test');
@@ -21,8 +22,39 @@ test('notifications created', async ({ page }) => {
 
   // todo - When runnning the test in VS Code, making the call below makes
   //   axios throw "Error: connect ECONNREFUSED ::1:443"
-  const response = await adminApi.get('/datasets');
-  expect(response.datasets.length).toBeGreaterThan(0);
+  // const response = await adminApi.get('/datasets');
+
+  const token = await page.evaluate(() => localStorage.getItem('token'));
+
+  console.log('token');
+  console.log(token);
+  //
+  // const response = await axios({
+  //   method: 'DELETE',
+  //   url: `https://localhost/api/datasets?is_duplicate=true`,
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //     'Content-Type': 'application/json',
+  //   },
+  //   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+  // });
+
+  const { request } = page;
+  const _apiResponse = await request.get(`${config.apiBasePath}/datasets`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    ignoreHTTPSErrors: true,
+    // httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+  });
+
+  const responseBody = await _apiResponse.json()
+
+  console.log('response')
+  console.log(responseBody)
+
+  expect(responseBody.datasets.length).toEqual(32);
 
   // todo - pre-test validation
   // await expect(page.getByTestId('notification-count-badge')).toText('0');
