0581e3ee78af76869b45b9d8a970a82a34403200
Rishi Pandey <ripandey29@gmail.com>
2024-05-03 16:16:54 -0400

    233 - create multiple notifications

--- tests/tests/view/authenticated/notifications/notifications.spec.js
+++ tests/tests/view/authenticated/notifications/notifications.spec.js
@@ -1,31 +1,23 @@
-const config = require('config');
-
+const _ = require('lodash');
 const { test, expect } = require('@playwright/test');
 
-const NOTIFICATION_LABEL = 'Notification Label';
-const NOTIFICATION_TEXT = 'Notification Text';
+const config = require('config');
 
-// todo
-//  tests for
-//   different roles
+const NUMBER_OF_NOTIFICATIONS = 2;
+const NOTIFICATION_LABELS = _.range(NUMBER_OF_NOTIFICATIONS)
+  .map((e, i) => `Notification Label ${i + 1}`);
+const NOTIFICATION_TEXTS = _.range(NUMBER_OF_NOTIFICATIONS)
+  .map((e, i) => `Notification Text ${i + 1}`);
 
 test.describe.configure({ mode: 'serial' });
 
 test.beforeEach(async ({ page }) => {
   // delete active notifications before each test
-
   await page.goto('/');
   const { request } = page;
-
   const token = await page.evaluate(() => localStorage.getItem('token'));
 
-  const deleteResponse = await request.delete(`${config.apiBasePath}/notifications?active=true`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    ignoreHTTPSErrors: true,
-  });
-  await deleteResponse.json();
+  await deleteActiveNotifications(request, token);
 });
 
 test.describe('Notifications', () => {
@@ -44,40 +36,81 @@ test.describe('Notifications', () => {
   });
 
   test('Notification created', async ({ page }) => {
-    let createdNotification;
+    let createdNotifications;
 
-    await test.step('Create a notification', async () => {
+    await test.step('Create notifications', async () => {
       await page.goto('/');
       const token = await page.evaluate(() => localStorage.getItem('token'));
 
       const { request } = page;
 
       // create a notification
-      const createResponse = await request.post(`${config.apiBasePath}/notifications`, {
-        data: {
-          label: NOTIFICATION_LABEL,
-          text: NOTIFICATION_TEXT,
-        },
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        ignoreHTTPSErrors: true,
-      });
-      createdNotification = await createResponse.json();
+      createdNotifications = await createNotifications({ request, token });
     });
 
     await test.step('Assert', async () => {
       const badgeTextLocator = page.getByTestId('notification-count')
         .locator('span.va-badge__text');
-      await expect(badgeTextLocator).toContainText('1');
+      await expect(badgeTextLocator).toContainText(`${NUMBER_OF_NOTIFICATIONS}`);
 
       // open the notification menu
       await page.getByTestId('notification-icon').click();
 
-      await expect(page.getByTestId(`notification-${createdNotification.id}-label`))
-        .toContainText(NOTIFICATION_LABEL);
-      await expect(page.getByTestId(`notification-${createdNotification.id}-text`))
-        .toContainText(NOTIFICATION_TEXT);
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < NUMBER_OF_NOTIFICATIONS; i++) {
+        // eslint-disable-next-line no-await-in-loop
+        await expect(page.getByTestId(`notification-${createdNotifications[i].id}-label`))
+          .toContainText(NOTIFICATION_LABELS[i]);
+        // eslint-disable-next-line no-await-in-loop
+        await expect(page.getByTestId(`notification-${createdNotifications[i].id}-text`))
+          .toContainText(NOTIFICATION_TEXTS[i]);
+      }
     });
   });
 });
+
+const createNotifications = async ({ request, token }) => {
+  const created = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < NUMBER_OF_NOTIFICATIONS; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    const notification = await createNotification(
+      {
+        request,
+        token,
+        label: NOTIFICATION_LABELS[i],
+        text: NOTIFICATION_TEXTS[i],
+      },
+    );
+    created.push(notification);
+  }
+
+  return created;
+};
+
+const createNotification = async ({
+  request, token, label, text,
+}) => {
+  const createResponse = await request.post(`${config.apiBasePath}/notifications`, {
+    data: {
+      label,
+      text,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    ignoreHTTPSErrors: true,
+  });
+  const createdNotification = await createResponse.json();
+  return createdNotification;
+};
+
+const deleteActiveNotifications = async (request, token) => {
+  const deleteResponse = await request.delete(`${config.apiBasePath}/notifications?active=true`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    ignoreHTTPSErrors: true,
+  });
+  await deleteResponse.json();
+};
