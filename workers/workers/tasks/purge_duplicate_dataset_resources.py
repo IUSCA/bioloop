415dc83f35e2ba2830a379ca0485d8d0bb149c8a
scadev@colo25 <scadev@colo25.carbonate.uits.iu.edu>
2024-05-07 13:20:37 -0400

    173 - fixed reject

--- workers/workers/tasks/purge_duplicate_dataset_resources.py
+++ workers/workers/tasks/purge_duplicate_dataset_resources.py
@@ -24,28 +24,6 @@ def purge(celery_task, duplicate_dataset_id, **kwargs):
 
     if not incoming_duplicate_dataset['is_duplicate']:
         raise InspectionFailed(f"Dataset {duplicate_dataset_id} is not a duplicate")
-    if incoming_duplicate_dataset['is_deleted']:
-        raise InspectionFailed(f"Dataset {duplicate_dataset_id} is deleted")
-
-    # matching_datasets = api.get_all_datasets(
-    #     name=incoming_duplicate_dataset['name'],
-    #     dataset_type=incoming_duplicate_dataset['type'],
-    #     is_duplicate=False,
-    #     deleted=False,
-    # )
-    # ensure system is not in an invalid state
-    # if len(matching_datasets) != 1:
-    #     raise InspectionFailed(
-    #         f"Expected to find one active (not deleted) original {incoming_duplicate_dataset['type']} named {incoming_duplicate_dataset['name']},"
-    #         f" but found {len(matching_datasets)}.")
-
-    # original_dataset = matching_datasets[0]
-
-    # if original_dataset['id'] != incoming_duplicate_dataset['duplicated_from']['original_dataset_id']:
-    #     raise InspectionFailed(f"Expected dataset {duplicate_dataset_id} to have "
-    #                            f"been duplicated from dataset "
-    #                            f"{incoming_duplicate_dataset['duplicated_from']['original_dataset_id']}, "
-    #                            f"but matching dataset has id {original_dataset['id']}.")
 
     rejected_dataset_latest_state = incoming_duplicate_dataset['states'][0]['state']
     if rejected_dataset_latest_state != config['DATASET_STATES']['DUPLICATE_REJECTED']:
