617da7d4202977642aeb5467857d0fdc5442651c
ri-pandey <ripandey29@gmail.com>
2024-08-23 21:51:02 +0000

    99 - verify upload scope on client side

--- secure_download/src/routes/upload.js
+++ secure_download/src/routes/upload.js
@@ -48,18 +48,21 @@ router.post(
       name, data_product_name, total, index, size, checksum, chunk_checksum,
     } = req.body;
 
-    // const UPLOAD_SCOPE = config.get('upload_scope');
+    const UPLOAD_SCOPE = config.get('upload_scope');
 
-    // const scopes = (req.token?.scope || '').split(' ');
-    // console.log(`scopes: ${scopes}`);
+    const scopes = (req.token?.scope || '').split(' ');
+    console.log(`scopes: ${scopes}`);
 
-    // const has_upload_scope = scopes.find((scope) => scope === UPLOAD_SCOPE);
+    const matching_scopes = scopes.filter((scope) => scope === UPLOAD_SCOPE);
     // console.log(`has_upload_scope: ${has_upload_scope}`);
 
-    // if (!has_upload_scope) {
-    //   return next(createError.Forbidden('Invalid scope'));
-    // }
-    // console.log('passed scope check');
+    if (matching_scopes.length === 0) {
+      return next(createError.Forbidden('Expected one, but found no matching scopes'))
+    }
+    if (matching_scopes.length !== 1) {
+      return next(createError.Forbidden('Exptected one, but found multiple matching scopes'));
+    }
+    console.log('passed scope check');
 
     if (!(data_product_name && checksum && chunk_checksum) || Number.isNaN(index)) {
       res.sendStatus(400);
@@ -68,38 +71,21 @@ router.post(
     // eslint-disable-next-line no-console
     console.log('Processing file piece ...', data_product_name, name, total, index, size, checksum, chunk_checksum);
 
-    // const filePath = `${getFileChunksStorageDir(req.body.data_product_name,
-    // req.body.checksum)}/${getFileChunkName(req.body.checksum,
-    // req.body.index)}`; const stats = await fsPromises.open(filePath, 'w');
-    // console.log('writing to file');
-    // while (stats.size < 2048000) {
-    // await fsPromises.appendFile(filePath, 'test ');
-    // }
-    // console.log('wrote to file');
 
-    // const receivedFilePath = req.file.path;
-    // fs.readFile(receivedFilePath, (err, data) => {
-    //   if (err) {
-    //     throw err;
-    //   }
-
-    // const evaluated_checksum =
-    // createHash('md5').update(data).digest('hex'); if (evaluated_checksum
-    // !== chunk_checksum) { res.sendStatus(409).json('Expected checksum for
-    // chunk did not equal evaluated checksum'); return; }
-
-    //   res.sendStatus(200);
-    // });
+    const evaluated_checksum = createHash('md5').update(data).digest('hex');
+    if (evaluated_checksum !== chunk_checksum) {
+      return next(createError.BadRequest('Evaluated checksum of chunk does not match checksum received in the request'));
+    }
 
     res.sendStatus(200);
-  }),
+  })
 );
 
-router.get(
-  '/test',
-  asyncHandler(async (req, res, next) => {
-    res.send('OK test');
-  }),
-);
+// router.get(
+//   '/test',
+//   asyncHandler(async (req, res, next) => {
+//     res.send('OK test');
+//   }),
+// );
 
 module.exports = router;
