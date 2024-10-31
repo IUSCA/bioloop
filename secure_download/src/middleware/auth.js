7c13bf43145aec33c04b6fbfed7c2f8ea8424a46
scadev@colo25 <scadev@colo25.carbonate.uits.iu.edu>
2024-10-31 10:45:28 -0400

    99 - removed WIP code

--- secure_download/src/middleware/auth.js
+++ secure_download/src/middleware/auth.js
@@ -4,25 +4,34 @@ const jsonwt = require('jsonwebtoken');
 const authService = require('../services/auth');
 
 function authenticate(req, res, next) {
-  const invalid_token_err = createError.Unauthorized('Authentication failed. Token is not valid.');
+  console.log('validating token ...');
 
   let token = req.query?.token;
+  // console.log('token', token);
+
   if (!token) {
     const authHeader = req.headers.authorization;
     if (!authHeader) return next(createError.Unauthorized('Authentication failed. Token not found.'));
     if (!authHeader.startsWith('Bearer ')) { return next(invalid_token_err); }
     // eslint-disable-next-line prefer-destructuring
     token = authHeader.split(' ')[1];
+    // console.log('token from header', token);
   }
 
   authService.checkJWT(token).then((decoded_token) => {
-    if (!decoded_token) return next(invalid_token_err);
+    if (!decoded_token) {
+      console.log('could not decode token');
+      return next(invalid_token_err)
+    };
     req.token = decoded_token;
+    console.log('token validated');
     next();
   }).catch((e) => {
+    console.log('jwt verification error', e);
     if (e instanceof jsonwt.TokenExpiredError) {
       return next(createError.Unauthorized('Authentication failed. Token expired.'));
     }
+    console.log('caught error, but error not instance of TokenExpiredError')
     next(e);
   });
 }
