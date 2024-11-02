f4d162df090d5127f5dfad497b6768ad32d81beb
scadev@bioloop-dev1 <scadev@bioloop-dev1.sca.iu.edu>
2024-11-02 19:38:22 +0000

    99 - revised migrations

---
+++ api/prisma/migrations/20241102193737_99_dataset_upload/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "upload_status" AS ENUM ('UPLOADING', 'UPLOAD_FAILED', 'UPLOADED', 'PROCESSING', 'PROCESSING_FAILED', 'COMPLETE', 'FAILED');
+
+-- AlterTable
+ALTER TABLE "dataset" ADD COLUMN     "file_type_id" INTEGER;
+
+-- CreateTable
+CREATE TABLE "dataset_file_type" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "extension" TEXT NOT NULL,
+
+    CONSTRAINT "dataset_file_type_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "upload_log" (
+    "id" SERIAL NOT NULL,
+    "status" "upload_status" NOT NULL,
+    "dataset_id" INTEGER,
+    "started" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "last_updated" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "upload_log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "file_upload_log" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "md5" TEXT NOT NULL,
+    "num_chunks" INTEGER NOT NULL,
+    "status" "upload_status" NOT NULL,
+    "path" TEXT,
+    "upload_log_id" INTEGER,
+
+    CONSTRAINT "file_upload_log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "dataset_file_type_name_extension_key" ON "dataset_file_type"("name", "extension");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "upload_log_dataset_id_key" ON "upload_log"("dataset_id");
+
+-- AddForeignKey
+ALTER TABLE "dataset" ADD CONSTRAINT "dataset_file_type_id_fkey" FOREIGN KEY ("file_type_id") REFERENCES "dataset_file_type"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "upload_log" ADD CONSTRAINT "upload_log_dataset_id_fkey" FOREIGN KEY ("dataset_id") REFERENCES "dataset"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "upload_log" ADD CONSTRAINT "upload_log_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "file_upload_log" ADD CONSTRAINT "file_upload_log_upload_log_id_fkey" FOREIGN KEY ("upload_log_id") REFERENCES "upload_log"("id") ON DELETE CASCADE ON UPDATE CASCADE;
