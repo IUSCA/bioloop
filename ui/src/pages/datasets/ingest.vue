930a04789eba151ce2f147ddc77b2d4791bcff88
Rishi Pandey <96927993+ri-pandey@users.noreply.github.com>
2024-11-22 16:29:13 -0500

    Added config to optionally disable dataset ingestions (#345)
    * disabled at UI-level
    
    * disabled at API-level

--- ui/src/pages/datasets/ingest.vue
+++ ui/src/pages/datasets/ingest.vue
@@ -2,7 +2,10 @@
   <div class="w-full flex justify-center">
     <va-card class="flex-auto max-w-5xl md:h-[calc(85vh)]">
       <va-card-content class="h-full">
-        <IngestionStepper />
+        <IngestionStepper v-if="isIngestionEnabled" />
+        <va-alert color="warning" icon="warning" v-else
+          >This feature is currently disabled</va-alert
+        >
       </va-card-content>
     </va-card>
   </div>
@@ -10,6 +13,16 @@
 
 <script setup>
 import { useNavStore } from "@/stores/nav";
+import config from "@/config";
+import { useAuthStore } from "@/stores/auth";
+
+const auth = useAuthStore();
+const { hasRole } = auth;
+
+const isIngestionEnabled =
+  config.enabledFeatures.ingestion.enabledForRoles.some((role) =>
+    hasRole(role),
+  );
 
 const nav = useNavStore();
 nav.setNavItems([
