ee4f2a98731fc07c950f7b60b93e382fd989b60b
Rishi Pandey <ripandey29@gmail.com>
2024-11-24 22:21:36 -0500

    173 - removed redundant files

--- restore_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /home/scadev/bioloop_env_reference/.env /opt/sca/bioloop/.env
-cp /home/scadev/bioloop_env_reference/ui/.env /opt/sca/bioloop/ui/.env
-cp /home/scadev/bioloop_env_reference/api/.env /opt/sca/bioloop/api/.env
