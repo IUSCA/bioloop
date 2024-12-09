0b96618f9d800f66f73aaee0d6facf2d79a419dd
Rishi Pandey <ripandey29@gmail.com>
2024-12-09 11:21:18 -0500

    173 - make task idempotent

--- workers/workers/tasks/purge_duplicate_dataset_resources.py
+++ workers/workers/tasks/purge_duplicate_dataset_resources.py
@@ -23,17 +23,20 @@ def purge(celery_task, duplicate_dataset_id, **kwargs):
                                                  include_duplications=True)
 
     if not incoming_duplicate_dataset['is_duplicate']:
-        raise InspectionFailed(f"Dataset {duplicate_dataset_id} is not a duplicate")
+        logger.warning(f"Dataset {duplicate_dataset_id} is not a duplicate")
+        return duplicate_dataset_id,
 
     rejected_dataset_latest_state = incoming_duplicate_dataset['states'][0]['state']
     if rejected_dataset_latest_state != config['DATASET_STATES']['DUPLICATE_REJECTED']:
-        raise InspectionFailed(f"Expected dataset {incoming_duplicate_dataset['id']} to be in state "
-                               f"{config['DATASET_STATES']['DUPLICATE_REJECTED']}, but current state is "
-                               f"{rejected_dataset_latest_state}.")
+        logger.warning(f"""
+        Expected dataset {incoming_duplicate_dataset['id']} to be in state
+        {config['DATASET_STATES']['DUPLICATE_REJECTED']}, but current state is
+        {rejected_dataset_latest_state}.
+        """)
+        return duplicate_dataset_id,
 
     duplicate_dataset_origin_path = Path(incoming_duplicate_dataset['origin_path']).resolve() if \
         incoming_duplicate_dataset['origin_path'] is not None else None
-
     if (duplicate_dataset_origin_path is not None and
             duplicate_dataset_origin_path.exists()):
         shutil.rmtree(duplicate_dataset_origin_path)
