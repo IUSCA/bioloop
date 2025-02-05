f0950d99c876527f551d073233b5f875e8f29501
Ryan Long <ryanlong@iu.edu>
2025-02-05 11:57:57 -0600

    Fix 99-autoreload.sh to ensure proper newline at end of file

--- nginx/bin/99-autoreload.sh
+++ nginx/bin/99-autoreload.sh
@@ -7,4 +7,5 @@ while :; do
     # Detect cert changes and reload if necessary.
     inotifywait -e close_write,moved_to,create /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem && \
     nginx -s reload
+    
 done &
