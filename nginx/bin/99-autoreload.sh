37f0d0becff1e9654bb2efa745ca729396c5d179
Ryan Long <ryanlong@iu.edu>
2025-02-07 13:16:07 -0600

    Update docker-compose.yml and 99-autoreload.sh for improved certificate renewal and Nginx reloading

--- nginx/bin/99-autoreload.sh
+++ nginx/bin/99-autoreload.sh
@@ -5,7 +5,6 @@ APP_DOMAIN=${APP_DOMAIN:-demo.bioloop.io}
 #!/bin/sh
 while :; do
     # Detect cert changes and reload if necessary.
-    inotifywait -e close_write,moved_to,create /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem && \
+    inotifywait /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem && \
     nginx -s reload
-    
 done &
