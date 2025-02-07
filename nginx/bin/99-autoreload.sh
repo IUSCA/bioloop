be6d92b9210c628de4dd56eb98d325222d87b681
Ryan Long <ryanlong@iu.edu>
2025-02-07 13:45:13 -0600

    Enhance 99-autoreload.sh to monitor certificate changes using file modification time and add a sleep interval before reloading Nginx

--- nginx/bin/99-autoreload.sh
+++ nginx/bin/99-autoreload.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
 # 99-autoreload.sh
 
 
 APP_DOMAIN=${APP_DOMAIN:-demo.bioloop.io}
+CERT_FILE="/etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem"
+LAST_MOD_TIME=$(stat -c %Y "$CERT_FILE")
 
-#!/bin/bash
 while :; do
-    # Detect cert changes and reload if necessary.
-    inotifywait /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem && \
-    nginx -s reload
+    CURRENT_MOD_TIME=$(stat -c %Y "$CERT_FILE")
+    if [ "$CURRENT_MOD_TIME" != "$LAST_MOD_TIME" ]; then
+        LAST_MOD_TIME=$CURRENT_MOD_TIME
+        nginx -s reload
+    fi
+    sleep 10
 done &
