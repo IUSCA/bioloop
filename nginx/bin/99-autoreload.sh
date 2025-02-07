244d2b5244bb4550bd0b3b64cab0f94473711f14
Ryan Long <ryanlong@iu.edu>
2025-02-07 15:34:55 -0600

    Update docker-compose.yml and 99-autoreload.sh for optimized certificate renewal and Nginx reloading intervals

--- nginx/bin/99-autoreload.sh
+++ nginx/bin/99-autoreload.sh
@@ -2,16 +2,9 @@
 
 # 99-autoreload.sh
 
-
-APP_DOMAIN=${APP_DOMAIN:-demo.bioloop.io}
-CERT_FILE="/etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem"
-LAST_MOD_TIME=$(stat -c %Y "$CERT_FILE")
-
+#!/bin/sh
 while :; do
-    CURRENT_MOD_TIME=$(stat -c %Y "$CERT_FILE")
-    if [ "$CURRENT_MOD_TIME" != "$LAST_MOD_TIME" ]; then
-        LAST_MOD_TIME=$CURRENT_MOD_TIME
-        nginx -t && nginx -s reload
-    fi
-    sleep 10
+    # Optional: Instead of sleep, detect config changes and only reload if necessary.
+    nginx -t && nginx -s reload
+    sleep 6h
 done &
