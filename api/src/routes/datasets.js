e0d4d7693fca842bac4f7374092f04a66db94d14
Rishi Pandey <ripandey29@gmail.com>
2025-03-05 23:21:57 -0500

    211 -WIP - UI changes

--- api/src/routes/datasets.js
+++ api/src/routes/datasets.js
@@ -734,7 +734,7 @@ router.get(
     if (dataset.metadata.stage_alias) {
       const download_file_path = isFileDownload
         ? `${dataset.metadata.stage_alias}/${file.path}`
-        : `${dataset.metadata.bundle_alias}`;
+        : `${dataset.bundle.name}`;
       const url = new URL(download_file_path, `${config.get('download_server.base_url')}`);
       // use url.pathname instead of download_file_path to deal with spaces in
       // the file path oauth scope cannot contain spaces
