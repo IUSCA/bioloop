4f51aeda9ad0feafeb59bd13cedc941495c659c3
Rishi Pandey <ripandey29@gmail.com>
2025-03-11 23:28:03 -0400

    397 - track origin space metrics

--- workers/workers/scripts/metrics.py
+++ workers/workers/scripts/metrics.py
@@ -3,6 +3,8 @@ Run by cron, collects disk usage information and sends to the API
 """
 
 import socket
+import os
+import shutil
 
 from workers import api
 from workers import hpfs
@@ -46,6 +48,39 @@ def main():
     # print(metrics)
     api.send_metrics(metrics)
 
+
+def persist_registration_metrics():
+    raw_data_archival_path = config['registration']['RAW_DATA']['source_dir']
+    data_product_archival_path = config['registration']['DATA_PRODUCT']['source_dir']
+
+    metrics = []
+    hostname = socket.getfqdn()
+
+    # Get metrics for raw_data_archival_path
+    total, used, free = shutil.disk_usage(raw_data_archival_path)
+
+    metrics.append({
+        'measurement': 'origin_space',
+        'subject': hostname,
+        'usage': used,
+        'limit': total,
+        'tags': []
+    })
+
+    # Get metrics for data_product_archival_path
+    total, used, free = shutil.disk_usage(data_product_archival_path)
+
+    metrics.append({
+        'measurement': 'data_product_archival',
+        'subject': hostname,
+        'usage': used,
+        'limit': total,
+        'free': free,
+        'tags': []
+    })
+
+    # Send metrics to API
+    api.send_metrics(metrics)
 
 if __name__ == '__main__':
     main()
