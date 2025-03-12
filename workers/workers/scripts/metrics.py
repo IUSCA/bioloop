5f92e8f76ad5b869aca6548a15c8b23ea1270358
scadev@colo25 <scadev@colo25.carbonate.uits.iu.edu>
2025-03-12 10:56:27 -0400

    397 - track raw data metrics

--- workers/workers/scripts/metrics.py
+++ workers/workers/scripts/metrics.py
@@ -11,6 +11,15 @@ from workers import hpfs
 from workers.config import config
 
 
+def get_directory_size(path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
 def main():
     metrics = []
     hostname = socket.getfqdn()
@@ -45,42 +54,39 @@ def main():
         ]
     )
 
+    registration_metrics = get_registration_metrics()
+
+    metrics.extend([registration_metrics['origin_space']])
+
     # print(metrics)
+    print("sending registration space metrics")
+
     api.send_metrics(metrics)
 
 
-def persist_registration_metrics():
+def get_registration_metrics():
     raw_data_archival_path = config['registration']['RAW_DATA']['source_dir']
-    data_product_archival_path = config['registration']['DATA_PRODUCT']['source_dir']
+    # data_product_archival_path = config['registration']['DATA_PRODUCT']['source_dir']
 
-    metrics = []
+    metrics = {}
     hostname = socket.getfqdn()
 
     # Get metrics for raw_data_archival_path
-    total, used, free = shutil.disk_usage(raw_data_archival_path)
+    used = get_directory_size(raw_data_archival_path)
 
-    metrics.append({
+    # print(f"used space: {used}")
+    # print(f"hostname: {hostname}")
+    # print(f"raw_data_archival_path: {raw_data_archival_path}")
+
+    metrics['origin_space'] = {
         'measurement': 'origin_space',
         'subject': hostname,
         'usage': used,
-        'limit': total,
+        'limit': None,
         'tags': []
-    })
+    }
 
-    # Get metrics for data_product_archival_path
-    total, used, free = shutil.disk_usage(data_product_archival_path)
-
-    metrics.append({
-        'measurement': 'data_product_archival',
-        'subject': hostname,
-        'usage': used,
-        'limit': total,
-        'free': free,
-        'tags': []
-    })
-
-    # Send metrics to API
-    api.send_metrics(metrics)
+    return metrics
 
 if __name__ == '__main__':
     main()
