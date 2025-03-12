96e50a9fdbb1fbadc433abd8c53a006cb091c110
Ryan Long <ryanlong@iu.edu>
2025-03-12 12:42:01 -0500

    fix generate_signet_env.sh: correct string comparison syntax for environment variable checks

--- bin/generate_signet_env.sh
+++ bin/generate_signet_env.sh
@@ -21,20 +21,24 @@ done < "signet/.env.example"
 for key in "${!env_vars[@]}"; do
   eval echo "$key"="\${$key}"
   
-  if [ $key == "POSTGRES_DB" ]; then
+  if [ "$key" == "POSTGRES_DB" ]; then
     eval echo "SIGNET_DB"="${SIGNET_DB}" >> $folder/signet.env
+    continue
   fi
 
-  if [ $key == "POSTGRES_USER" ]; then
+  if [ "$key" == "POSTGRES_USER" ]; then
     eval echo "SIGNET_USER"="${SIGNET_USER}" >> $folder/signet.env
+    continue
   fi
 
-  if [ $key == "POSTGRES_PASSWORD" ]; then
+  if [ "$key" == "POSTGRES_PASSWORD" ]; then
     eval echo "SIGNET_PASSWORD"="${SIGNET_PASSWORD}" >> $folder/signet.env
+    continue
   fi
 
-  if [ $key == "POSTGRES_HOST" ]; then
+  if [ "$key" == "POSTGRES_HOST" ]; then
     eval echo "SIGNET_DB_HOST"="${SIGNET_DB_HOST}" >> $folder/signet.env
+    continue
   fi
 
   eval echo "$key"="\${$key}" >> $folder/signet.env
