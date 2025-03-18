fe10a4c83d16bf173abb5d2d27be429c782d1fbf
Ryan Long <ryanlong@iu.edu>
2025-03-18 12:39:51 -0500

    fix(deploy): implement non-privileged user setup with configurable UID and GID in Dockerfiles and entrypoint scripts

---
+++ workers/bin/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Default UID and GID
+TARGET_UID=${UID:-1001}
+TARGET_GID=${GID:-1001}
+
+# Update the group ID if necessary
+if [ "$(id -g scauser)" != "$TARGET_GID" ]; then
+    groupmod -g "$TARGET_GID" scauser
+fi
+
+# Update the user ID if necessary
+if [ "$(id -u scauser)" != "$TARGET_UID" ]; then
+    usermod -u "$TARGET_UID" scauser
+fi
+
+# Ensure ownership of the working directory
+chown -R scauser:scauser /opt/sca
+
+# Execute the main command as the non-privileged user
+exec gosu scauser "$@"
