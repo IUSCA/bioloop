77ca9db3c2f363cc4a560737f976d6a2794560e0
Ryan Long <ryanlong@iu.edu>
2025-03-18 10:35:30 -0500

    fix(deploy): update startup script to check for existing keys and add volume for api_keys

--- api/bin/startup.sh
+++ api/bin/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ ! -f keys/auth.pub ] || [ ! -f keys/auth.key ]; then
+if [ -f keys/auth.pub ] && [ -f keys/auth.key ]; then
   echo "Keys not found. Generating keys..."
   cd keys && ./genkeys.sh 
+  cd .. 
 else
   echo "Keys already exist. Skipping key generation." 
 fi
 
-cd .. 
+
 npx prisma generate 
 npm run start
