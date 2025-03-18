7a03c158ea3d990b4334a9abd29c00c75f322ce3
Ryan Long <ryanlong@iu.edu>
2025-03-18 14:18:23 -0500

    fix(deploy): streamline non-privileged user setup in Dockerfiles and entrypoint scripts

--- workers/bin/entrypoint.sh
+++ workers/bin/entrypoint.sh
@@ -4,16 +4,9 @@
 TARGET_UID=${USER_ID:-1001}
 TARGET_GID=${GROUP_ID:-1001}
 
-# Update the group ID if necessary
-if [ "$(id -g scauser)" != "$TARGET_GID" ]; then
-    groupmod -g "$TARGET_GID" scauser
-fi
-
-# Update the user ID if necessary
-if [ "$(id -u scauser)" != "$TARGET_UID" ]; then
-    usermod -u "$TARGET_UID" scauser
-fi
+groupadd -g $TARGET_GID scauser
+useradd -m -u $TARGET_UID -g $TARGET_GID scauser
 
 
 # Execute the main command as the non-privileged user
-exec "$@"
+exec su -s /bin/bash scauser -c "$@"
