5bc86086fc85e08e9f3026618d6be595afd1fd54
Ryan Long <ryanlong@iu.edu>
2025-03-18 10:40:30 -0500

    fix(deploy): update key generation process in startup script to use OpenSSL

--- api/bin/startup.sh
+++ api/bin/startup.sh
@@ -2,7 +2,10 @@
 
 if [ -f keys/auth.pub ] && [ -f keys/auth.key ]; then
   echo "Keys not found. Generating keys..."
-  cd keys && ./genkeys.sh 
+  cd keys 
+  openssl genrsa -out auth.key 2048
+  chmod 600 auth.key
+  openssl rsa -in auth.key -pubout > auth.pub
   cd .. 
 else
   echo "Keys already exist. Skipping key generation." 
