afda11a19e68f7629fddf762c4a015fa38bb1329
Rishi Pandey <ripandey29@gmail.com>
2025-03-20 01:24:30 -0400

    - Add edit toggle functionality for directory selection mode - Implement local state management for dataset name
    TODO:
    - test

--- ui/src/components/dataset/upload/UploadedDatasetName.vue
+++ ui/src/components/dataset/upload/UploadedDatasetName.vue
@@ -1,14 +1,22 @@
 <template>
-  <CopyText v-if="props.selectingDirectory" :text="props.datasetName" />
-
   <va-input
-    v-else-if="props.selectingFiles"
-    v-model="datasetNameInput"
+    v-model="localDatasetName"
     :placeholder="'Dataset name'"
     class="w-full"
     :messages="'Name for the uploaded dataset'"
-    :disabled="props.inputDisabled"
-  />
+    :disabled="isInputDisabled"
+  >
+    <template #append v-if="props.selectingDirectory">
+      <div class="flex gap-2">
+        <va-button
+          :icon="isEditing ? 'check' : 'edit'"
+          size="small"
+          @click="handleEditToggle"
+        />
+        <CopyButton :text="localDatasetName" preset="secondary" />
+      </div>
+    </template>
+  </va-input>
 
   <div class="va-text-danger text-xs" v-if="props.datasetNameError">
     {{ props.datasetNameErrorMessages }}
@@ -16,6 +24,8 @@
 </template>
 
 <script setup>
+import { computed, ref, watch } from 'vue';
+
 const props = defineProps({
   datasetName: {
     type: String,
@@ -49,12 +59,40 @@ const props = defineProps({
 
 const emit = defineEmits(["update:datasetNameInput", "update:datasetName"]);
 
-const datasetNameInput = computed({
-  get() {
-    return props.datasetNameInput;
-  },
-  set(value) {
-    emit("update:datasetNameInput", value);
-  },
+const isEditing = ref(false);
+const localDatasetName = ref(props.selectingDirectory ? props.datasetName : props.datasetNameInput);
+
+const isInputDisabled = computed(() => {
+  return props.selectingDirectory ? !isEditing.value : props.inputDisabled;
 });
+
+watch(() => props.datasetName, (newValue) => {
+  if (props.selectingDirectory && !isEditing.value) {
+    localDatasetName.value = newValue;
+  }
+});
+
+watch(() => props.datasetNameInput, (newValue) => {
+  if (!props.selectingDirectory) {
+    localDatasetName.value = newValue;
+  }
+});
+
+watch(localDatasetName, (newValue) => {
+  // if (props.selectingDirectory) {
+  //   emit("update:datasetName", newValue);
+  // } else {
+    emit("update:datasetNameInput", newValue);
+  // }
+});
+
+const handleEditToggle = () => {
+  if (props.selectingDirectory) {
+    isEditing.value = !isEditing.value;
+    if (!isEditing.value) {
+      // After editing, propagate updated dataset name
+      emit("update:datasetNameInput", localDatasetName.value);
+    }
+  }
+};
 </script>
