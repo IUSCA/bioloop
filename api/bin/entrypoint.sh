0d27f751ca96f067587c86333b41a1fe88da21a4
Ryan Long <ryanlong@iu.edu>
2025-04-02 15:47:44 -0500

    feat: add Dockerfiles and environment configuration for application setup

---
+++ api/bin/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if keys exists
+if [ -f keys/auth.pub ] && [ -f keys/auth.key ]; then
+  echo "Keys already exist. Skipping key generation." 
+else
+  echo "Keys not found. Generating keys..."
+  cd keys 
+  openssl genrsa -out auth.key 2048
+  chmod 600 auth.key
+  openssl rsa -in auth.key -pubout > auth.pub
+  cd .. 
+fi
+
+# Generate Prisma client
+npx prisma generate 
+
+# Run database migrations
+npx prisma migrate deploy
+
+# Run the application
+$*
--- workers/workers/scripts/start_worker.sh
+++ workers/workers/scripts/start_worker.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-python -m celery \
-  -A workers.celery_app worker \
-  --loglevel INFO \
-  -O fair \
-  --pidfile celery_worker.pid \
-  --hostname 'bioloop-celery-w1@%h' \
-  --autoscale 8,3 \
-  --queues 'bioloop-dev.sca.iu.edu.q' 
-  # --detach
+
+if [ $WORKER_TYPE == "celery_worker" ]; then
+  echo "Starting Celery Worker"
+  python -m celery \
+    -A workers.celery_app worker \
+    --loglevel INFO \
+    -O fair \
+    --pidfile celery_worker.pid \
+    --hostname 'bioloop-celery-w1@%h' \
+    --autoscale 8,3 \
+    --queues 'bioloop-dev.sca.iu.edu.q' 
+      # --detach
+elif [ $WORKER_TYPE == "watch" ]; then
+  echo "Starting Watch Worker"
+  python -m workers.scripts.watch
+elif [ $WORKER_TYPE == "metrics" ]; then
+  echo "Starting Metrics Worker"
+  python -m workers.scripts.metrics
+elif [ $WORKER_TYPE == "purge_staged_datasets" ]; then
+  echo "Starting Purge Staged Datasets Worker"
+  python -m workers.scripts.purge_staged_datasets
+elif [ $WORKER_TYPE == "purge_stale_workflows" ]; then
+  echo "Starting Purge Stale Workflows Worker"
+  python -m workers.scripts.purge_stale_workflows
+elif [ $WORKER_TYPE == "manage_pending_dataset_uploads" ]; then
+  echo "Starting Manage Pending Dataset Uploads Worker"
+  python -m workers.scripts.manage_pending_dataset_uploads
+elif [ $WORKER_TYPE == "process_upload_dataset" ]; then
+  echo "Starting Process Upload Dataset Worker"
+  python -m workers.scripts.process_upload_dataset
+else
+  echo "Invalid Worker Type"
+fi
