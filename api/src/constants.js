1cb3cde4bba35f6aecb4c16087324bcbdfca3787
Rishi Pandey <ripandey29@gmail.com>
2025-10-07 15:05:21 -0400

    fixed include

--- api/src/constants.js
+++ api/src/constants.js
@@ -103,16 +103,7 @@ const INCLUDE_DATASET_UPLOAD_LOG_RELATIONS = {
           },
           projects: {
             select: {
-              id: true,
-              name: true,
-              slug: true,
-              description: true,
-              browser_enabled: true,
-              funding: true,
-              metadata: true,
-              created_at: true,
-              updated_at: true,
-              owner_id: true,
+              project: true,
             },
           },
         },
