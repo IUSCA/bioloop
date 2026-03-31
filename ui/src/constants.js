e24f3f66a5d75f6714470401b3d24a50a59b6e4a
Rishi Pandey <96927993+ri-pandey@users.noreply.github.com>
2026-03-31 09:31:43 -0400

    Create history page for Datasets-Import (#567)

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -16,7 +16,7 @@ const exports = {
             feature_key: "import",
             icon: "mdi-file-import-outline",
             title: "Import",
-            path: "/datasets/import",
+            path: "/datasets/imports",
           },
           {
             feature_key: "uploads",
---
+++ ui/src/pages/datasets/imports/new.vue
@@ -0,0 +1,41 @@
+<template>
+  <va-alert
+    color="warning"
+    icon="warning"
+    v-if="!auth.isFeatureEnabled('import')"
+  >
+    This feature is currently disabled
+  </va-alert>
+
+  <div v-else class="w-full flex justify-center">
+    <va-card class="flex-auto md:max-w-5xl md:h-[calc(85vh)]">
+      <va-card-content class="h-full">
+        <ImportStepper />
+      </va-card-content>
+    </va-card>
+  </div>
+</template>
+
+<script setup>
+import ImportStepper from "@/components/dataset/import/ImportStepper.vue";
+import { useAuthStore } from "@/stores/auth";
+import { useNavStore } from "@/stores/nav";
+
+const nav = useNavStore();
+const auth = useAuthStore();
+
+nav.setNavItems([
+  {
+    label: "Dataset Imports",
+    to: "/datasets/imports",
+  },
+  {
+    label: "Import New",
+  },
+]);
+</script>
+
+<route lang="yaml">
+meta:
+  title: Dataset Imports
+</route>
