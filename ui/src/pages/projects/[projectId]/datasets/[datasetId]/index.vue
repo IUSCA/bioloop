6cd74e7801237ae37ee033cd4facdc45a4f415e0
elijah <elijahkim45@gmail.com>
2026-08-11 20:38:15 -0400

    refactor: pass page-fetched dataset into Dataset and FileBrowser

--- ui/src/pages/projects/[projectId]/datasets/[datasetId]/index.vue
+++ ui/src/pages/projects/[projectId]/datasets/[datasetId]/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <Dataset :dataset-id="props.datasetId" append-file-browser-url />
+  <Dataset
+    v-if="dataset"
+    v-model:dataset="dataset"
+    append-file-browser-url
+  />
 </template>
 
 <script setup>
 import config from "@/config";
 import DatasetService from "@/services/dataset";
 import projectService from "@/services/projects";
+import toast from "@/services/toast";
 import { useAuthStore } from "@/stores/auth";
 import { useNavStore } from "@/stores/nav";
 import { useUIStore } from "@/stores/ui";
@@ -16,33 +21,46 @@ const ui = useUIStore();
 
 const props = defineProps({ projectId: String, datasetId: String });
 
+const dataset = ref(null);
+
 Promise.all([
   projectService.getById({
     id: props.projectId,
     forSelf: !auth.canOperate,
   }),
-  DatasetService.getById({ id: props.datasetId }),
-]).then((results) => {
-  const project = results[0].data;
-  const dataset = results[1].data;
-  nav.setNavItems([
-    {
-      label: "Projects",
-      to: `/projects`,
-    },
-    {
-      label: project.name,
-      to: `/projects/${project.slug}`,
-    },
-    {
-      label: config.dataset.types[dataset.type]?.label,
-    },
-    {
-      label: dataset.name,
-    },
-  ]);
-  ui.setTitle(project.name);
-});
+  DatasetService.getById({
+    id: props.datasetId,
+    bundle: true,
+    initiator: true,
+    include_source_instrument: true,
+  }),
+])
+  .then((results) => {
+    const project = results[0].data;
+    dataset.value = results[1].data;
+    nav.setNavItems([
+      {
+        label: "Projects",
+        to: `/projects`,
+      },
+      {
+        label: project.name,
+        to: `/projects/${project.slug}`,
+      },
+      {
+        label: config.dataset.types[dataset.value.type]?.label,
+      },
+      {
+        label: dataset.value.name,
+      },
+    ]);
+    ui.setTitle(project.name);
+  })
+  .catch((err) => {
+    console.error(err);
+    if (err?.response?.status == 404) toast.error("Could not find the dataset");
+    else toast.error("Could not fetch dataset");
+  });
 </script>
 
 <route lang="yaml">
